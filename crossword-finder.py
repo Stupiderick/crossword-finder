9da43a2cac606521933dcca542faf2678009f2b7
Erick Li <ericklek1994@gmail.com>
2017-09-30 22:14:41 -0500

    First minor

--- crossword-finder.py
+++ crossword-finder.py
@@ -4,3 +4,8 @@ def word_length():
     '''
     Get the length of the word that users want to find.
     '''
+
+def word_position():
+    '''
+    Ask the users to specify the known letter(s).
+    '''
